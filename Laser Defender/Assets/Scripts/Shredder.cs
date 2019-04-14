c3c6d599149d642e3e3f9516823e8e2dbae1059b
Phil Davidson <pxdavidson@msn.com>
2019-04-14 10:18:31 +0100

    Loads of stuff, I forgot but checking in
    Added shredder script that uses OnTriggerEnter to Destroy(gameObject)
    Added Enemy controller and waypoints
    Added scriptable object that allows the waypoints to be set and pulled from a List<>

---
+++ Laser Defender/Assets/Scripts/Shredder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+    // Detect collision
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Shred(collision);
+    }
+
+
+    // Destroys whatever gameObject hitsthe collider
+    private void Shred(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
---
+++ Laser Defender/Assets/Scripts/WaveScriptableObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveScriptableObject : ScriptableObject
+{
+    // Declare variables
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject enemyPathPrefab;
+    [SerializeField] float enemySpawnRate = 0.5f;
+    [SerializeField] float spawnRND = 0.2f;
+    [SerializeField] int numberOfEnemies = 5;
+    [SerializeField] float enemyMoveSpeed = 2;
+
+    // Return called variable
+    public GameObject   GetEnemyPrefab()      { return enemyPrefab; }
+    public float        GetEnemySpawnRate()   { return enemySpawnRate; }
+    public float        GetSpawnRND()         { return spawnRND; }
+    public int          GetNumberOfEnemies()  { return numberOfEnemies; }
+    public float        GetEnemyMoveSpeed()   { return enemyMoveSpeed; }
+
+    // Return list of waypoints from attached prefab
+    public List<Transform> GetWaypoints()
+    {
+        var waveWaypoints = new List<Transform>();
+        foreach (Transform child in enemyPathPrefab.transform)
+        {
+            waveWaypoints.Add(child);
+        }
+        return waveWaypoints;
+    }
+}
