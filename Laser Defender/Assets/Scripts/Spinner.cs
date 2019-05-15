8f1eb55c8b8c301635bb3ee449e0d13a5f586181
Phil Davidson <pxdavidson@msn.com>
2019-05-15 20:11:57 +0100

    Projectile tidy
    - Set render order to draw projectiles under player/enemy ships
    - Added Spinner.cs to add some rotation noise to the Laser Bomb 1

---
+++ Laser Defender/Assets/Scripts/Spinner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    // Declare variables
+    [SerializeField] float rotationRate = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 0f, rotationRate / Time.deltaTime);
+    }
+}
